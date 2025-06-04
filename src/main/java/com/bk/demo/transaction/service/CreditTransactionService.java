4e62d77db0b59d473e89f6ef95b5cd2c376ecb4b
HMummadi <hemanth.vthu@gmaill.com>
2025-06-04 17:25:40 -0400

    Modified to read transactions from csv and generate logs from application

--- src/main/java/com/bk/demo/transaction/service/CreditTransactionService.java
+++ src/main/java/com/bk/demo/transaction/service/CreditTransactionService.java
@@ -3,24 +3,27 @@ package com.bk.demo.transaction.service;
 import com.bk.demo.transaction.model.CreditTransaction;
 import com.bk.demo.transaction.repository.CreditTransactionRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.time.ZonedDateTime;
 import java.util.List;
 
 @Service
 @Slf4j
 public class CreditTransactionService {
+    private static final Logger log = LoggerFactory.getLogger(CreditTransactionService.class);
     private final CreditTransactionRepository repository;
 
     public CreditTransactionService(CreditTransactionRepository repository) {
         this.repository = repository;
     }
 
-    public CreditTransaction createTransaction(CreditTransaction transaction) {
-        log.info("Attempting to save transaction with id: {}", transaction.getId());
-        transaction.setTransactionDate(ZonedDateTime.now());
-        return repository.save(transaction);
+    public CreditTransaction createTransaction(CreditTransaction creditTransaction) {
+        log.info("Attempting to save transaction");
+        CreditTransaction creditTransaction1 = repository.save(creditTransaction);
+        log.info("Transaction saved successfully");
+        return creditTransaction1;
     }
 
     public List<CreditTransaction> getAllTransactions() {
